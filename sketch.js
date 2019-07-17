6f6f6630d91e700904e9d1a99d323a90d28f89eb
AkinolaASC6 <akinpedro90@gmail.com>
2019-07-17 12:06:54 -0400

    first commit

---
+++ sketch.js
@@ -0,0 +1,27 @@
+let x = 0;
+function setup(){
+    createCanvas(1000,1000);
+    noLoop();
+
+}
+
+function draw(){
+    background(0, 0, 255)
+ square(0,0, 100);
+
+}
+
+// for ( let i= 0; i<10;i++){
+//     function draw(){
+//         square(xPos + 100,0,100);
+//     }
+// }
+
+
+
+
+
+
+
+
+
